4acc1785ed4fe392d22cb2dfc9833341862c6dcf
BhagiaSheri <bhagiasheri24@gmail.com>
2023-02-01 10:56:04 -0600

    minor refactoring in Lab # 7 - react task

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -12,11 +12,15 @@ function Dashboard() {
         ]
     );
 
-    const inputRef = useRef();
+    const [title, setTitle] = useState(postsState[0].title)
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+      };
     
     const handleOnClick = () => {
         const newPostsState = [...postsState];
-        newPostsState[0].title = inputRef.current.value;
+        newPostsState[0].title = title;
         setPostsState(newPostsState);
     };
 
@@ -29,13 +33,14 @@ function Dashboard() {
             <div>
                 <h3>CHANGE TITLE OF FIRST POST</h3>
                 <input
-                    ref={inputRef}
-                    key={'201'}
+                    key={postsState[0].id}
                     type="text"
                     label={'title'}
                     name={'title'}
+                    value= {title}
+                    onChange={handleChange}
                 />
-                <button onClick={handleOnClick()}>Update Title</button>
+                <button onClick={handleOnClick}>Update Title</button>
             </div>
         </>
     )
